f1038b36bfce3c64ad3b0703a8ed6724aa4c721f
PaigeSylvan <paigesylvan@gmail.com>
2023-12-15 21:56:31 -0600

    done but not styled

---
+++ deploy-demo/src/App.js
@@ -0,0 +1,13 @@
+
+import App from './components/App';
+
+function App2() {
+  return (
+    <div className="App">
+      <App/>
+
+    </div>
+  );
+}
+
+export default App2;
---
+++ deploy-demo/src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Todo extends React.Component {
+  render() {
+    const { id, name, completed } = this.props.todo
+    const { toggleCompletion} = this.props
+    return (
+      <div onClick={ () => toggleCompletion(id)}>
+        {name} {completed && '✔️'}
+      </div>
+    )
+  }
+}
